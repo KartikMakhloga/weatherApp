e643958fba5cacf95bbeb643c1d4e2286dd52046
Kartik Makhloga <kartikmakhoga20@gmail.com>
2024-02-17 13:09:14 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import { ToastContainer} from 'react-toastify';
+
+function MyApp({ Component, pageProps }) {
+  return <>
+    <Component {...pageProps} />
+    <ToastContainer />
+  </>
+}
+
+export default MyApp
